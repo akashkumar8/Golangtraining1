845ff11b67a3a400cc44eadafd05b9e008ef1c61
Akash Kumar <67784046+akashkumar8@users.noreply.github.com>
2020-10-24 12:49:31 +0530

    for as a range

---
+++ range.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	rvariable := []string{"KLD", "Kloudone Official", "KloudoneLearning"}
+
+	for i, j := range rvariable {
+		fmt.Println(i, j)
+	}
+
+}

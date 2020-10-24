8bceef3bda254fe0d0fd096d8216c4ec28838253
Akash Kumar <67784046+akashkumar8@users.noreply.github.com>
2020-10-24 12:00:41 +0530

    updates on function

--- funcnw1.go
+++ funcnw1.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
     superman()
     result := multiplyme(3,6)
-    fmt.Println(result)
+	fmt.Println(result)
+	myesult, mylength, myname := adder(3, 6, 4, 2, 4, 12, 23)
+	fmt.Println(myresult, mylenth, myname)
 
 }
 
@@ -17,3 +19,15 @@ func multiplyme(v1, v2 int) int {
     return v1 * v2
     
 }
+
+func adder)values ...int) (int, int, string) {
+	sum := 0
+	for i:= range values {
+		sum = sum + values[i]
+	}
+	length := len(values)
+	name := "just for fun"
+
+	return sum, length, name
+}
+
